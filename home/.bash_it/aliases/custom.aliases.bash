c99ab4ed7842ac7af9f71d75c3f97899c9c527a2
Carlos Zubieta <carlos.zubieta@hp.com>
2014-09-04 17:53:11 -0500

    Added custom aliases to bash_it

---
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -0,0 +1,4 @@
+alias msf="sudo mount -t vboxsf Virtualbox_Shared_Folder /home/zubieta/VBShared/"
+alias ipnb="cd ~/Documents/Notebooks && ipython notebook --pylab=inline"
+alias reload="source ~/.bashrc"
+alias quit="exit"
