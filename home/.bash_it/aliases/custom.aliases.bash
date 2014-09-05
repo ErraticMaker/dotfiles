02ea6a2f762ac6f36262340a8f19c48962fb9833
Carlos Zubieta <carlos.zubieta@hp.com>
2014-09-05 16:23:39 -0500

    md alias for mkdir -p

--- home/.bash_it/aliases/custom.aliases.bash
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -2,3 +2,4 @@ alias msf="sudo mount -t vboxsf Virtualbox_Shared_Folder /home/zubieta/VBShared/
 alias ipnb="cd ~/Documents/Notebooks && ipython notebook --pylab=inline"
 alias reload="source ~/.bashrc"
 alias quit="exit"
+alias md="mkdir -p"
